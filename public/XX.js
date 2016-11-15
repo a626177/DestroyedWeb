b7f32c203f69400867aa694a73d90481c3ac0eae
黃祥斌 <mxn36@yahoo.com.tw>
2016-11-15 21:55:41 +0800

    愛哲哲

--- public/XX.js
+++ public/XX.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
   $("#butt").click(function(){
     $.get({
       url:'/ajax',
-      data:{ id: $('#ID').val()},
+      data:{ 
+        id: $('#ID').val(),
+        pwd:$('#passW').val(),
+
+      },
       success: function(res) {
         console.log(res)
+        $('#res').html('saveed')
       }
     })
     
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express') //express加進來
+const app = express()//使用他
 app.use(express.static(__dirname + '/public'))
-app.listen(8866)
+app.listen(7788)
 
-app.get('/', (req, res) => {
+var store = []
+var fs = require('fs')
+app.get('/', (req, res) => {//一條斜線的門
   res.sendfile('public/index.html')
 })
 
 app.get('/ajax', (req, res) => {
-  console.log(req.query.id)//印出資料
-  res.send(req.query.id + " ajax")
+  console.log(store)//印出資料
+  store.push(req.query)
+  fs.writeFile("mem.json", JSON.stringify(store), function(err) {
+      if(err) throw (err)
+      console.log("The file was saved!")
+  })
+  res.send(req.query.id )
 })
